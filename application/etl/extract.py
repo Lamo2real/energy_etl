b3904c3cc75608ba0a16d72e2fcec053ba00a0d9
Lamo2real <lamochi02@gmail.com>
2025-05-25 15:04:53 +0200

    more logging for extract subscript

--- application/etl/extract.py
+++ application/etl/extract.py
@@ -1,12 +1,12 @@
 
 
-
 import requests
 import os
 from dotenv import load_dotenv
 import pandas as pd
 from log import setup_logger
 logger = setup_logger()
+
 
 def extract_energy_data() -> pd.DataFrame:
     """
@@ -15,11 +15,11 @@ def extract_energy_data() -> pd.DataFrame:
     return a Dataframe out of it
     """
 
-    try:    
+    try:
         load_dotenv()
         api_key = os.environ.get('API_KEY')
 
-    except FileNotFoundError as fnfe:
+    except FileNotFoundError:
         logger.error('file to .env was not found, check if it exists')
         raise
     except Exception as e:
@@ -27,16 +27,16 @@ def extract_energy_data() -> pd.DataFrame:
         raise
 
     endpoint = (
-        f"https://api.eia.gov/v2/electricity/retail-sales/data/"
-        f"?frequency=monthly"
-        f"&sort[0][column]=period&sort[0][direction]=asc"
-        f"&data[0]=customers"
-        f"&data[1]=price"
-        f"&data[2]=revenue"
-        f"&data[3]=sales"
-        f"&start=2001-01&end=2001-12"
-        f"&length=5000"
-        f"&api_key={api_key}"
+        f'https://api.eia.gov/v2/electricity/retail-sales/data/'
+        f'?frequency=monthly'
+        f'&sort[0][column]=period&sort[0][direction]=asc'
+        f'&data[0]=customers'
+        f'&data[1]=price'
+        f'&data[2]=revenue'
+        f'&data[3]=sales'
+        f'&start=2001-01&end=2001-12'
+        f'&length=5000'
+        f'&api_key={api_key}'
     )
 
     ###### assign variable ######
@@ -45,24 +45,26 @@ def extract_energy_data() -> pd.DataFrame:
     try:
         data = requests.get(endpoint)
 
-    except req_exc.Timeout:
-        logger.error('request timeout')
-        raise
-    except req_exc.ConnectionError:
-        logger.error('connection to API FAILED')
-        raise
-    except req_exc.HTTPError as http_error:
-        logger.error(f"HTTP error ocurred: {http_error}")
-        raise
-    except req_exc.RequestException as re:
-        logger.error(f"An error ocurred while making the request: {re}")
+    except (
+        req_exc.Timeout, req_exc.ConnectionError,
+        req_exc.HTTPError, req_exc.RequestException, Exception
+    ) as e:
+        logger.error(f'Exception occurred: {e}', exc_info=True)
         raise
 
-    json_data = data.json()
-    raw_df = pd.json_normalize(json_data['response']['data'])
+    try:
+        json_data = data.json()
+        raw_df = pd.json_normalize(json_data['response']['data'])
+
+    except KeyError as ke:
+        logger.error(ke, exc_info=True)
+        raise
+    except Exception as e:
+        logger.error(f'unexpected error: {e}', exc_info=True)
+        raise
+
     # print(raw_df.head(10))
     return raw_df
-    
 
 
 if __name__ == '__main__':
